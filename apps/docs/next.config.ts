d8ad4f6d6ad45dd51e6cb6496def3814bb07db80
robbiecren07 <rcrenshawdev@gmail.com>
2026-04-28 14:52:32 -0400

    fix(docs): CSP value cause broken UI

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -26,10 +26,6 @@ const nextConfig: NextConfig = {
             key: 'Strict-Transport-Security',
             value: 'max-age=63072000; includeSubDomains; preload',
           },
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self'; object-src 'none';",
-          },
           { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
         ],
       },
